429be09102661b9a1d306a49b56d4af10fc9807a
Zack Ankner <zackankner@gmail.com>
2020-06-05 04:42:22 -0400

    feat(master) navbar and sticking to it

--- src/components/header.js
+++ src/components/header.js
@@ -3,17 +3,18 @@ import React from "react"
 import { Nav, Navbar } from "react-bootstrap"
 
 const Header = () => (
-  <Navbar bg="white" expand="md">
-    <Navbar.Brand href="/">Test</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="ml-auto">
-        <Nav.Link href='/' className="nav-item">Home</Nav.Link>
-        <Nav.Link href='/' className="nav-item">Home</Nav.Link>
-        <Nav.Link href='/' className="nav-item">Home</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+  <div className="container">
+    <Navbar bg="white" expand="lg">
+      <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/link">Link</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </div>
 );
 
 export default Header;
