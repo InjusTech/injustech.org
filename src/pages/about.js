1d47fdb74aa9993cb349401e9815c11b4a6326d8
Jack Weiler <jackweiler@Jacks-MacBook-Pro-3.local>
2020-06-07 13:42:01 -0400

    all about changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Row, Col, Container } from "reactstrap"
 import Layout from "../components/layout"
 import "../styles/about.css"
+import Image from "react-bootstrap/Image"
 
 const AboutPage = () => (
   <Layout>
@@ -19,7 +20,16 @@ const AboutPage = () => (
           d="M0,192L80,186.7C160,181,320,171,480,170.7C640,171,800,181,960,165.3C1120,149,1280,107,1360,85.3L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
         ></path>
       </svg>
-      <div class="first-strip">hey</div>
+      <div class="first-strip">
+        <div id="first-strip-text">
+          Digital integration is integral for growing businesses in the 21st
+          century, but the growth of black entrepeneurship has remained fairly
+          stagnant through the Dot-Com era. By removing the cost and time
+          bottleneck to get on the internet, we hope to help that that 9% number
+          skyrocket.
+        </div>
+        <Image id="first-strip-img" src={require("../images/chart.png")} />
+      </div>
     </div>{" "}
     <Container>
       <Row className="py-5"></Row>
