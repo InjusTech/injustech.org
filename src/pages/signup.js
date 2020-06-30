554dd4b212c98334305e62d6bb45028d96e23d5e
Zack Ankner <zackankner@gmail.com>
2020-06-30 14:18:44 -0400

    feat(master) deployable

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,31 +1,40 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import SignUpForm from "../components/sign-form"
 import Layout from "../components/layout"
 
-const SignUp = ({ location }) => (
-  <Layout>
-    <section className="landing-section">
-      <div className="container">
-        <h1 className="mb-4">Sign Up Your Business</h1>
-        <div className="row">
-          <div className="col-md-6">
-            <div>
-              <SignUpForm email={window.history.state.email}></SignUpForm>
+const SignUp = ({ location }) => {
+
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    setEmail(window.history.state.email);
+  });
+
+  return (
+    <Layout>
+      <section className="landing-section">
+        <div className="container">
+          <h1 className="mb-4">Sign Up Your Business</h1>
+          <div className="row">
+            <div className="col-md-6">
+              <div>
+                <SignUpForm email={email}/>
+              </div>
             </div>
-          </div>
-          <div className="col-md-6">
-            <div className="mt-4 ml-4">
-              <img
-                alt="forms"
-                className="img-fluid mt-4"
-                src={require("../images/fill-forms.png")}
-              />
+            <div className="col-md-6">
+              <div className="mt-4 ml-4">
+                <img
+                  alt="forms"
+                  className="img-fluid mt-4"
+                  src={require("../images/fill-forms.png")}
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  </Layout>
-)
+      </section>
+    </Layout>
+  );
+};
 
 export default SignUp
